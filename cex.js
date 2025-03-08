70a3c9824769a87ac895f84b509f29bd3f2190d9
Linx-ESP <github@linxesp.com>
2025-03-08 19:21:32 +0100

    añadir cex.js

---
+++ cex.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         CEX URL Filter
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  Añade automáticamente filtros de disponibilidad a la URL de CEX
+// @author       Linx
+// @match        https://es.webuy.com/search*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    // Verificar si la URL ya contiene el parámetro de disponibilidad
+    if (!window.location.href.includes('availability=')) {
+        // Construir la nueva URL con el parámetro de disponibilidad
+        const separator = window.location.href.includes('?') ? '&' : '?';
+        const newUrl = window.location.href + separator + "availability=Disponible+online~Disponible+en+tienda";
+        
+        // Redirigir a la nueva URL
+        window.location.href = newUrl;
+    }
+})();
